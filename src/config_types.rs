eeb0856cd70110962f2be87eed3897e4935fcdfd
Travis Finkenauer <tmfinken@gmail.com>
2021-05-07 22:18:44 -0700

    Support shell-quoting for program args

--- src/config_types.rs
+++ src/config_types.rs
@@ -4,10 +4,9 @@ use std::str::FromStr;
 pub struct ProgArgs(Vec<String>);
 
 impl FromStr for ProgArgs {
-    type Err = String;
+    type Err = &'static str;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // todo(tmfink): handle shell quoting
-        let args: Vec<String> = s.split_whitespace().map(|s| s.to_string()).collect();
+        let args = shlex::split(s).ok_or("Invalid shell escaped string")?;
         Ok(Self(args))
     }
 }
@@ -29,5 +28,42 @@ impl FromStr for SocketType {
             "udp" | "dgram" => Ok(Self::Udp),
             _ => Err("Invalid input: must be tcp|udp (or alises stream|dgram)"),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn socket_type() {
+        assert_eq!("tcp".parse::<SocketType>(), Ok(SocketType::Tcp));
+        assert_eq!("UDP".parse::<SocketType>(), Ok(SocketType::Udp));
+        assert!("blah".parse::<SocketType>().is_err());
+    }
+
+    #[test]
+    fn prog_args() {
+        assert_eq!("".parse::<ProgArgs>(), Ok(ProgArgs(vec![])));
+        assert_eq!(
+            "simple".parse::<ProgArgs>(),
+            Ok(ProgArgs(vec!["simple".to_string()]))
+        );
+        assert_eq!(
+            "\t\t  arg1 arg2\targ3  ".parse::<ProgArgs>(),
+            Ok(ProgArgs(vec![
+                "arg1".to_string(),
+                "arg2".to_string(),
+                "arg3".to_string()
+            ]))
+        );
+        assert_eq!(
+            r#"'arg1 with spaces' arg2\ with\ \ spaces "with quotes""#.parse::<ProgArgs>(),
+            Ok(ProgArgs(vec![
+                "arg1 with spaces".to_string(),
+                "arg2 with  spaces".to_string(),
+                "with quotes".to_string(),
+            ]))
+        );
     }
 }
